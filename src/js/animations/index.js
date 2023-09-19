1e359ed6a588ebda71051036c29779bec9400301
jonozzle <jonathan.cole.1994@gmail.com>
2023-09-19 20:47:48 +1200

    Revert "yarn changes"
    This reverts commit 501a4ec7df654dd9b80bc84e86729fd6fd2b1178.

--- src/js/animations/index.js
+++ src/js/animations/index.js
@@ -2,6 +2,7 @@ export { default as slideup } from './slideup.js';
 export { default as slideup_end } from './slideup_end.js';
 export { default as slidedown } from './slidedown.js';
 export { default as stick } from './stick.js';
+//export { default as elevatetext } from './elevatetext.js';
 export { default as lettershuffle } from './lettershuffle.js';
 export { default as imagebounce } from './imagebounce.js';
 export { default as MouseTrail } from './mousetrail.js';
